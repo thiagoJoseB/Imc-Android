180e68b46811d84eb0586b6dda92ed0d311cc676
thiago bueno <thiagobueno.tbo@gmail.com>
2021-10-26 16:59:23 -0300

    atualizacao

--- app/src/main/java/com/example/imcapp/MainActivity.kt
+++ app/src/main/java/com/example/imcapp/MainActivity.kt
@@ -2,12 +2,27 @@ package com.example.imcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.cadastro)
 
-        supportActionBar!!.hide()
+      //  supportActionBar!!.hide()
+
+
+
+
+
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.cadastro, menu)
+
+
+        return true
     }
 }
