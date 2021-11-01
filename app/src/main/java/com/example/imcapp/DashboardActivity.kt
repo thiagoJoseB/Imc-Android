692c7a2df010926a5a1ecb47e0df08abdc4027b6
thiago bueno <thiagobueno.tbo@gmail.com>
2021-11-01 16:58:46 -0300

    atualizacao dia 01/11/

--- app/src/main/java/com/example/imcapp/DashboardActivity.kt
+++ app/src/main/java/com/example/imcapp/DashboardActivity.kt
@@ -1,11 +1,20 @@
 package com.example.imcapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        val buttonCriarConta = findViewById<Button>(R.id.button_criar_conta)
+
+        buttonCriarConta.setOnClickListener { val abrirCadastro = Intent(this, CadastroUsuarioActivity::class.java)
+        startActivity(abrirCadastro)
+        }
+
     }
 }
